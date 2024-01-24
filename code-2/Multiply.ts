e91a9535c1e44c83223a8c0b2f8ad5245402aa52
indra <indrasaputraidrus@gmail.com>
2024-01-24 13:06:30 +0800

    Code-3 - Return negative

--- code-2/Multiply.ts
+++ code-2/Multiply.ts
@@ -1,1 +1,3 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004
+
 export const multiply = (a: number, b: number): number => a * b
---
+++ code-3/ReturnNegative.ts
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+export const makeNegative = (num: number): number => num < 0 ? num : num * -1
+
+console.log(makeNegative(0))
